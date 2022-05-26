3cc7910a911ea57e443e3700cb2a3632011e647c
dimitar-paskov <dimitarp2002@abv.bg>
2022-05-26 11:17:02 +0300

    git ignore

---
+++ Exercises/Exercise07HeapsBST/07. Data-Structures-Heaps-BST-Exercises-Skeleton/src/main/java/solutions/Pair.java
@@ -0,0 +1,29 @@
+/**
+ * @author dimitar
+ *
+ */
+package solutions;
+
+public class Pair<K,V> {
+
+	private K key;
+	private V value;
+	
+	public Pair(K key, V value) {
+		this.key = key;
+		this.value = value;
+	}
+
+	public K getKey() {
+		return key;
+	}
+
+	public V getValue() {
+		return value;
+	}
+	
+	
+	
+	
+	
+}
